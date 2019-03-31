4f599efa70c7aa90fd202a89d0a252b54f260ed4
Neema2018 <you@example.com>
2019-03-31 08:49:23 +0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+var answers = ["infinity","firstName","vowels","script","head section"];
+var pointPerCorrect = 25;
+
+function percentage(score) {
+return "Your score is " + parseInt((score / 100) * 100) + "%";
+}
+
+$(document).ready(function(){
+   $("#questions").submit(function (event) {
+
+
+      $('#result').text('');
+      var score = 0;
+      var answerOne = ($("input[type=radio][name=questionOneAnswer]:checked").val());
+      var answerTwo = ($("input[type=radio][name=questionTwoAnswer]:checked").val());
+      var answerThree = ($("input[type=radio][name=questionThreeAnswer]:checked").val());
+      var answerFour = ($("input[type=radio][name=questionFourAnswer]:checked").val());
+      var answerFive = ($("input[type=radio][name=questionFiveAnswer]:checked").val());
+
+      if (answerOne === undefined || answerTwo === undefined || answerThree === undefined || answerFour === undefined  || answerFive === undefined) {
+  $('#questionsIncomplete').text('Please answer all questions before Submitting');
+  $('#questionsIncomplete').fadeOut(10000);
+}
+   else {
+         if (answerOne === answers[0]) {
+    score += pointPerCorrect;
+  }
+         if (answerTwo === answers[1]) {
+    score += pointPerCorrect;
+  }
+         if (answerThree === answers[2]) {
+    score += pointPerCorrect;
+  }
+  if (answerFour === answers[3]) {
+    score += pointPerCorrect;
+  }
+  if (answerFive === answers[4]) {
+    score += pointPerCorrect;
+  }
+
+
+        $("input[type=radio][name=questionOneChoice]:checked").prop('checked', false);
+        $("input[type=radio][name=questionTwoChoice]:checked").prop('checked', false);
+        $("input[type=radio][name=questionThreeChoice]:checked").prop('checked', false);
+        $("input[type=radio][name=questionFourChoice]:checked").prop('checked', false);
+        $("input[type=radio][name=questionFiveChoice]:checked").prop('checked', false);
+        $('#questionsIncomplete').text('');
+        $('#result').text(percentage(score));
+}
+          event.preventDefault();
+ });
+});
